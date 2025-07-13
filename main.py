541a230bd8a5812ca678931bf16cfd0b612e174f
Vladimir <663vova@gmail.com>
2025-07-13 18:38:04 +0200

    микроисправляния

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from database import DatabaseManager
